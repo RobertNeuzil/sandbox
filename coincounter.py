66f6d858755ba3cf06eb8b4dade31b10c96a6cd3
Robert Neuzil <robertneuzil@gmail.com>
2020-09-14 02:47:35 -0400

    Coin Counter
    A coin counter to help Alex count his piggy bank

---
+++ coincounter.py
@@ -0,0 +1,18 @@
+print ("Only Enter Numerical Values")
+
+pennies = int(input("Enter amount of pennies: "))
+
+nickels = int(input("Enter amount of nickels: "))
+
+dimes = int(input("Enter amount of dimes: "))
+
+quarters = int(input("Enter amount of quarters: "))
+
+pennies = pennies * 0.01
+nickels = nickels * 0.05
+dimes = dimes * 0.10
+quarters = quarters * 0.25
+
+rawtotal = pennies + nickels + dimes + quarters
+
+print ("You have " + "$" + str(rawtotal) + " dollars")
